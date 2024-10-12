30d48d14e09a78242cfd9528204a2a5ba1b866fd
User <User@DESKTOP-IM299NP>
2024-10-11 21:16:56 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace teste3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite números a serem somados");
+
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"O resultado da soma é {Soma(numero1, numero2)}");
+
+            Console.WriteLine("Aperte Enter para finalizar o programa");
+
+            Console.ReadLine();
+        }
+
+        public static int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
